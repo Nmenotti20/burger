c649c64c379cc07f82ffdc66a7ac4cfa09ecee96
Nickolas Menotti <nmenotti@cox.net>
2021-01-07 13:34:30 -0800

    built schema.sql and seeds.sql and added connection.js and npm

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 var mysql = require("mysql");
 
+// Your password
+var password = require("./password");
+    // Then you can call the password with this code:
+    // password.getPassword()
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: password.getPassword(),
   database: "burger_db"
 });
 
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -16,4 +22,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -4,20 +4,12 @@ CREATE DATABASE burger_db;
 
 USE burger_db;
 
-CREATE TABLE clients
+CREATE TABLE a
 (
-	id int NOT NULL AUTO_INCREMENT,
-	client_name varchar(255) NOT NULL,
-	PRIMARY KEY (id)
+
 );
 
-CREATE TABLE parties
+CREATE TABLE b
 (
-	id int NOT NULL AUTO_INCREMENT,
-	party_name varchar(255) NOT NULL,
-	party_type varchar(255) NOT NULL,
-	party_cost int NOT NULL,
-	client_id int NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (client_id) REFERENCES clients(id)
+
 );
